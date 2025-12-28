ef13fe05e9a70b3264f94607e0358d88b4712f83
vijay-papanaboina <vijaypapanaboina3@gmail.com>
2025-12-28 10:53:50 +0530

    ci: Add GitHub Actions workflow for GCP MIG deployment, update `/wait` endpoint message, and increment package version.

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
   burnCpu(1000); // adjust to control CPU usage
   res
     .status(200)
-    .send("waited for 1 second. Image built with cloud build with rollback templates");
+    .send("waited for 1 second. Image built with cloud build with rollback templates using replacement method substitute");
 });
 
 /**
